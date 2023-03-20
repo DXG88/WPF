a2c40274173f64ea2dcdef231f8be5bce3f42d2b
DXG88 <128389002+DXG88@users.noreply.github.com>
2023-03-20 21:55:33 +0800

    Add files via upload

---
+++ WpfApp1/RelayCommand.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WpfApp1
+{
+    public class RelayCommand : ICommand
+    {
+        public RelayCommand(Action<object> action)
+        {
+            DoExecute = action;
+        }
+
+        public event EventHandler? CanExecuteChanged;
+        public Func<object, bool>? CanExecution { set; get; }
+        public Action<object>? DoExecute { set; get; }
+
+        /// <summary>
+        /// IsEnabled="False" 就不会发起命令操作
+        /// </summary>
+        /// <param name="parameter"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public bool CanExecute(object? parameter)
+        {
+            if (CanExecution != null)
+            {
+                CanExecute(parameter);
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// 真正执行命令的方法
+        /// </summary>
+        /// <param name="parameter"></param>
+        /// <exception cref="NotImplementedException"></exception>
+        public void Execute(object? parameter)
+        {
+            DoExecute!.Invoke(parameter!);
+        }
+    }
+}
---
+++ WpfApp1/UserInfoViewModel.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Windows;
+
+namespace WpfApp1
+{
+    public class UserInfoViewModel : INotifyPropertyChanged
+    {
+        private User user;
+
+        public UserInfoViewModel()
+        {
+            user = new User();
+            SaveCommand = new RelayCommand(Save);
+            CancelCommand = new RelayCommand(Cancel);
+        }
+
+        public string UserName
+        {
+            get { return user.Name; }
+            set
+            {
+                user.Name = value;
+                OnPropertyChanged("UserName");
+            }
+        }
+
+        public int UserAge
+        {
+            get { return user.Age; }
+            set
+            {
+                user.Age = value;
+                OnPropertyChanged("UserAge");
+            }
+        }
+
+        public ICommand SaveCommand { get; private set; }
+        public ICommand CancelCommand { get; private set; }
+
+        private void Save(object parameter)
+        {
+            // Save user data to database or service
+            MessageBox.Show("User data saved!");
+
+            // Close dialog window
+            var window = parameter as Window;
+            if (window != null)
+                window.Close();
+
+        }
+
+        private void Cancel(object parameter)
+        {
+            // Close dialog window without saving data
+            var window = parameter as Window;
+            if (window != null)
+                window.Close();
+
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
